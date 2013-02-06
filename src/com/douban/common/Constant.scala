43611653ef5b36a50e98dcbb56997221feec764f
Joseph <jinntrance@gmail.com>
2013-02-06 22:03:10 +0800

    add new styles

---
+++ src/com/douban/common/Constant.scala
@@ -0,0 +1,14 @@
+package com.douban.common
+
+/**
+ * Copyright by <a href="http://crazyadam.net"><em><i>Joseph J.C. Tang</i></em></a> <br/>
+ * Email: <a href="mailto:jinntrance@gmail.com">jinntrance@gmail.com</a>
+ * @author joseph
+ * @since 1/26/13 9:01 PM
+ * @version 1.0
+ */
+object Constant {
+   val accessTokenString="accessToken"
+   val refreshTokenString="refreshToken"
+   val userIdString="userId"
+}
--- src/com/douban/ui/LoginActivity.scala
+++ src/com/douban/ui/LoginActivity.scala
@@ -1,4 +1,5 @@
-package com.douban.ui
+package com.douban
+package ui
 
 import android.os.Bundle
 import android.webkit.{WebViewClient, WebView}
@@ -6,8 +7,7 @@ import android.graphics.Bitmap
 import android.app.Activity
 import com.douban.R
 import com.douban.common._
-import com.douban.common.AccessToken
-import com.douban.common.AuthorizationCode
+import Auth._
 
 /**
  * Copyright by <a href="http://crazyadam.net"><em><i>Joseph J.C. Tang</i></em></a> <br/>
@@ -23,20 +23,24 @@ class LoginActivity extends Activity{
     setContentView(R.layout.auth)
     wv=findViewById(R.id.webView).asInstanceOf[WebView]
     wv.setWebViewClient(new DoubanWebViewClient)
-    wv.loadUrl(AuthorizationCode().authUrl)
+    val a=new AuthorizationCode()
+    wv.loadUrl(a.authUrl)
 
   }
 }
 class DoubanWebViewClient extends WebViewClient{
-  override def onPageStarted(view: WebView, url: String, favicon: Bitmap) {
-    if (url.startsWith(Auth.redirect_url)) {
-      val token: Token = new AccessToken(Auth.extractCode(url), Auth.redirect_url)
+  override def onPageStarted(view: WebView, redirectedUrl: String, favicon: Bitmap) {
+    if (redirectedUrl.startsWith(Auth.redirect_url)) {
+      val token:Token = new AccessToken(Auth.extractCode(redirectedUrl),redirect_url)
       view.stopLoading()
       val url = token.tokenUrl
       val t=Req.post[AccessTokenResult](url, token) //TODO to handle the error code
       Auth.access_token=t.get.access_token  //to store the access token and the refresh token
       Auth.refresh_token=t.get.refresh_token
+      Context.put(Constant.accessTokenString,t.get.access_token)
+      Context.put(Constant.refreshTokenString,t.get.refresh_token)
+      Context.put(Constant.userIdString,t.get.douban_user_id)
     }
-    else super.onPageStarted(view, url, favicon)
+    else super.onPageStarted(view, redirectedUrl, favicon)
   }
 }
