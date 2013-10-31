d32da39517589d787cb7e5a44dc84425329cb661
jinntrance <jinntrance@gmail.com>
2013-10-31 15:09:58 +0800

    notes

--- src/com/douban/book/MyNoteActivity.scala
+++ src/com/douban/book/MyNoteActivity.scala
@@ -65,7 +65,7 @@ class MyNoteActivity extends DoubanActivity{
     )
   }
   def viewNote(pos:Int){
-    startActivityForResult(SIntent[NotesActivity].putExtra(Constant.ARG_POSITION,pos).putExtra(Constant.DATA_LIST,new util.ArrayList(listAdapter.getList)),REQUEST_CODE)
+    startActivityForResult(SIntent[MyNoteViewActivity].putExtra(Constant.ARG_POSITION,pos).putExtra(Constant.DATA_LIST,new util.ArrayList(listAdapter.getData)),REQUEST_CODE)
   }
 
   override def onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
